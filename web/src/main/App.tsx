226600703dd5964d5bdf929eb3375dd5240c9f80
Ramakrushna Mishra <ram2mishra@gmail.com>
2023-07-24 21:58:05 +0530

    APi ready

---
+++ web/src/main/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+function App() {
+  return (
+   <div className="container">
+    <Header subtitle="A react app to simplify Bidding"></Header>
+   </div>
+  );
+}
+
+export default App;
